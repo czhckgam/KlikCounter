9272c2661adedae14c9c4b421f5dbf5ba21da657
examgenerator <examgenerator@marekblazek.cz>
2018-04-02 18:07:36 +0200

    Initial commit of the source code

---
+++ _src/KlikCounter/Player.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Media;
+
+namespace KlikCounter
+{
+    public class Player
+    {
+        //PUBLIC METHODS
+        /// <summary>
+        /// Plays Soviet anthem by Console.Beep() in a new thread
+        /// </summary>
+        public void PlaySovietAnthem()
+        {
+            Thread thread = new Thread(SovietAnthem);
+            thread.Start();
+        }
+
+        public void Yaa()
+        {
+            SoundPlayer player = new SoundPlayer("../../YARP/yaa1.wav");
+            player.Play();
+        }
+
+        public void YaaYaa()
+        {
+            SoundPlayer player = new SoundPlayer("../../YARP/yaa2.wav");
+            player.Play();
+        }
+
+        public void Nice()
+        {
+            SoundPlayer player = new SoundPlayer("../../YARP/nice.wav");
+            player.Play();
+        }
+
+        // PRIVATE METHODS
+
+        private void SovietAnthem()
+        {
+            //Console.Beep(385, 250);
+            Console.Beep(392, 250);
+            Console.Beep(523, 375);
+            Console.Beep(392, 375);
+            Console.Beep(440, 250);
+            Console.Beep(493, 375);
+            Console.Beep(329, 250);
+        }
+    }
+}
---
+++ _src/KlikCounter/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+namespace KlikCounter
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Pade();
+        }
+        private static void Pade()
+        {
+            Player player = new Player();
+            player.YaaYaa();
+            Console.Clear();
+            Console.WriteLine("Zadej počet kliků:");
+            double kliky;
+            try
+            {
+                kliky = double.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                kliky = 50;
+                Console.WriteLine("Neumíš zadat číslo. Dej si PADE!");
+                Console.ReadKey();
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+            double done = 0;
+            Console.Clear();
+            Console.WriteLine(@"Stiskem A započítáš klik 
+Stiskem S odebereš klik 
+Stiskem Q ukončíš
+Stiskem 5 přidáš PADE
+Stiskem R začneš znova");
+            while (true)
+            {
+                string selection = Console.ReadKey().KeyChar.ToString().ToUpper();
+                Console.WriteLine();
+                switch (selection)
+                {
+                    case "S":
+                        if (done < 1)
+                        {
+                            Console.WriteLine("Má hovno");
+                        }
+                        else
+                        {
+                            done--;
+                            WriteStatus(kliky, done);
+                            Console.Beep(550, 500);
+                        }
+                        break;
+                    case "Q":
+                        Console.WriteLine("Vííííííídřená bojovka");
+                        Console.ReadKey();
+                        Environment.Exit(0);
+                        break;
+                    case "A":
+                        if (done < kliky - 1)
+                        {
+                            done++;
+                            WriteStatus(kliky, done);
+                            if (done % 10 == 0)
+                                player.Yaa();
+                        }
+                        else
+                        {
+                            done = kliky;
+                            for (int i = 0; i < 10; i++)
+                            {
+                                YouShouldGetPade();
+                                DateTime dateTime = DateTime.Now;
+                                bool timeOut = false;
+                                while (!timeOut)
+                                {
+                                    TimeSpan time = DateTime.Now - dateTime;
+                                    if (time > new TimeSpan(0, 0, 1))
+                                        timeOut = true;
+                                }
+                            }
+                            YouShouldGetPade(true);
+                        }
+                        break;
+                    case "5":
+                        if (done == kliky)
+                        {
+                            done = 0;
+                            kliky = 50;
+                        }
+                        else
+                            kliky = kliky + 50;
+                        Console.WriteLine("Již jen: {0}",kliky-done);
+                        player.Nice();
+                        break;
+                    case "R":
+                        Pade();
+                        break;
+                    case "D":
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(@"
+                                          
+                        ,@,                    
+                       ,@@@,               
+                      ,@@@@@,              
+               `@@@@@@@@@@@@@@@@@@@`       
+                 `@@@@@@@@@@@@@@@`         
+                   `@@@@@@@@@@@`           
+                  ,@@@@@@`@@@@@@,          
+                  @@@@`     `@@@@         
+                 ;@`           `@;        
+                   _   _   _   _          
+                  (   (   (   |_)         
+                   ~   ~   ~  |
+");
+                        player.PlaySovietAnthem();
+                        break;
+
+                }
+            }
+
+        }
+
+        private static void WriteStatus(double kliky, double done)
+        {
+            Console.WriteLine("Už máš {0}", done);
+            Console.WriteLine("Již jen {0}", kliky - done);
+        }
+
+        private static void YouShouldGetPade(bool white = false)
+        {
+            if (!white)
+            {
+                if (Console.ForegroundColor == ConsoleColor.White || Console.ForegroundColor == ConsoleColor.Green)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            Console.Clear();      
+            Console.WriteLine("Hotovo, ale zasloužil by sis PADE\nPRO PŘIČTENÍ PADE STISKNI 5 ");
+        }
+    }
+}
